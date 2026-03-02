ed3601bd8980ccf7936e99f8728fb85f17dd59b6
luigi-lentini <luigi.lentini@libero.it>
2026-03-02 08:16:42 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { useState, useEffect } from 'react'
+import Login from './pages/Login.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Clienti from './pages/Clienti.jsx'
+import Attivita from './pages/Attivita.jsx'
+import Trattative from './pages/Trattative.jsx'
+import Layout from './components/Layout.jsx'
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem('token')
+  return token ? children : <Navigate to="/login" />
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={
+          <PrivateRoute>
+            <Layout />
+          </PrivateRoute>
+        }>
+          <Route index element={<Dashboard />} />
+          <Route path="clienti" element={<Clienti />} />
+          <Route path="attivita" element={<Attivita />} />
+          <Route path="trattative" element={<Trattative />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
